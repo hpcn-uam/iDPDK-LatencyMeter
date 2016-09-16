abf6566a66d312dbc3c91b60501b62a532a89d8c
Rafael Leira Osuna <rafael.leira@uam.es>
2016-09-16 17:11:38 +0200

    removed some workers and flushes (old) implementation

--- src/runtime.c
+++ src/runtime.c
@@ -125,6 +125,18 @@
 #define APP_IO_TX_PREFETCH1(p)
 #endif
 
+uint8_t icmppkt []={
+0x00, 0x1b, 0x21, 0xad, 0xa9, 0x9c, 0x14, 0xdd, 0xa9, 0xd2, 0xef, 0x57, 0x08, 0x00, 0x45, 0x00,
+0x00, 0x54, 0x51, 0x36, 0x40, 0x00, 0x40, 0x01, 0x6b, 0xee, 0x96, 0xf4, 0x3a, 0x72, 0xd8, 0x3a,
+0xd3, 0xe3, 0x08, 0x00, 0xeb, 0xe1, 0x66, 0x02, 0x00, 0x1a, 0x67, 0x72, 0x97, 0x57, 0x00, 0x00,
+0x00, 0x00, 0xe4, 0x64, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
+0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25,
+0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35,
+0x36, 0x37
+};
+
+int icmppktlen = sizeof(icmppkt);
+
 static inline void
 app_lcore_io_rx_buffer_to_send (
 	struct app_lcore_params_io *lp,
@@ -346,38 +358,6 @@ app_lcore_io_rx(
 }
 
 static inline void
-app_lcore_io_rx_flush(struct app_lcore_params_io *lp, uint32_t n_workers)
-{
-	uint32_t worker;
-
-	for (worker = 0; worker < n_workers; worker ++) {
-		int ret;
-
-		if (likely((lp->rx.mbuf_out_flush[worker] == 0) ||
-		           (lp->rx.mbuf_out[worker].n_mbufs == 0))) {
-			lp->rx.mbuf_out_flush[worker] = 1;
-			continue;
-		}
-
-		ret = rte_ring_sp_enqueue_bulk(
-			lp->rx.rings[worker],
-			(void **) lp->rx.mbuf_out[worker].array,
-			lp->rx.mbuf_out[worker].n_mbufs);
-
-		if (unlikely(ret < 0)) {
-			uint32_t k;
-			for (k = 0; k < lp->rx.mbuf_out[worker].n_mbufs; k ++) {
-				struct rte_mbuf *pkt_to_free = lp->rx.mbuf_out[worker].array[k];
-				rte_pktmbuf_free(pkt_to_free);
-			}
-		}
-
-		lp->rx.mbuf_out[worker].n_mbufs = 0;
-		lp->rx.mbuf_out_flush[worker] = 1;
-	}
-}
-
-static inline void
 app_lcore_io_tx(
 	struct app_lcore_params_io *lp,
 	uint32_t n_workers,
@@ -385,17 +365,6 @@ app_lcore_io_tx(
 	uint32_t bsz_wr)
 {
 	uint32_t worker;
-
-	const uint8_t icmppkt []={
-	0x00, 0x1b, 0x21, 0xad, 0xa9, 0x9c, 0x14, 0xdd, 0xa9, 0xd2, 0xef, 0x57, 0x08, 0x00, 0x45, 0x00,
-	0x00, 0x54, 0x51, 0x36, 0x40, 0x00, 0x40, 0x01, 0x6b, 0xee, 0x96, 0xf4, 0x3a, 0x72, 0xd8, 0x3a,
-	0xd3, 0xe3, 0x08, 0x00, 0xeb, 0xe1, 0x66, 0x02, 0x00, 0x1a, 0x67, 0x72, 0x97, 0x57, 0x00, 0x00,
-	0x00, 0x00, 0xe4, 0x64, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
-	0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25,
-	0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35,
-	0x36, 0x37
-	};
-	const int icmppktlen = 98;
 
 	for (worker = 0; worker < n_workers; worker ++) {
 		uint32_t i;
@@ -425,6 +394,8 @@ app_lcore_io_tx(
 			n_mbufs = 1;
 
 			struct rte_mbuf * tmpbuf = rte_ctrlmbuf_alloc(app.pools[0]) ;
+			if(!tmpbuf)
+				continue;
 			tmpbuf->pkt_len = icmppktlen;
 			tmpbuf->data_len = icmppktlen;
 			tmpbuf->port = port;
@@ -455,39 +426,6 @@ app_lcore_io_tx(
 	}
 }
 
-static inline void
-app_lcore_io_tx_flush(struct app_lcore_params_io *lp)
-{
-	uint8_t port;
-
-	for (port = 0; port < lp->tx.n_nic_ports; port ++) {
-		uint32_t n_pkts;
-
-		if (likely((lp->tx.mbuf_out_flush[port] == 0) ||
-		           (lp->tx.mbuf_out[port].n_mbufs == 0))) {
-			lp->tx.mbuf_out_flush[port] = 1;
-			continue;
-		}
-
-		n_pkts = rte_eth_tx_burst(
-			port,
-			0,
-			lp->tx.mbuf_out[port].array,
-			(uint16_t) lp->tx.mbuf_out[port].n_mbufs);
-
-		if (unlikely(n_pkts < lp->tx.mbuf_out[port].n_mbufs)) {
-			uint32_t k;
-			for (k = n_pkts; k < lp->tx.mbuf_out[port].n_mbufs; k ++) {
-				struct rte_mbuf *pkt_to_free = lp->tx.mbuf_out[port].array[k];
-				rte_pktmbuf_free(pkt_to_free);
-			}
-		}
-
-		lp->tx.mbuf_out[port].n_mbufs = 0;
-		lp->tx.mbuf_out_flush[port] = 1;
-	}
-}
-
 static void
 app_lcore_main_loop_io(void)
 {
@@ -502,13 +440,6 @@ app_lcore_main_loop_io(void)
 	uint8_t pos_lb = app.pos_lb;
 
 	for ( ; ; ) {
-		if (APP_LCORE_IO_FLUSH && (unlikely(i == APP_LCORE_IO_FLUSH))) {
-			if (likely(lp->rx.n_nic_queues > 0)) {
-				app_lcore_io_rx_flush(lp, n_workers); 
-			}
-			i = 0;
-		}
-
 		if (likely(lp->rx.n_nic_queues > 0)) {
 			app_lcore_io_rx(lp, n_workers, bsz_rx_rd, bsz_rx_wr, pos_lb); 
 		}
@@ -516,122 +447,6 @@ app_lcore_main_loop_io(void)
 		if (likely(lp->tx.n_nic_ports > 0)) {
 			app_lcore_io_tx(lp, 1, app.burst_size_io_tx_read, app.burst_size_io_tx_write); 
 		}
-
-		i ++;
-	}
-}
-
-static inline void
-app_lcore_worker(
-	struct app_lcore_params_worker *lp,
-	uint32_t bsz_rd)
-{
-	uint32_t i;
-
-	for (i = 0; i < lp->n_rings_in; i ++) {
-		struct rte_ring *ring_in = lp->rings_in[i]; //posible false sharing
-		uint32_t j;
-		int ret;
-
-		ret = rte_ring_sc_dequeue_bulk(
-			ring_in,
-			(void **) lp->mbuf_in.array,
-			bsz_rd);
-
-		if (unlikely(ret == -ENOENT)) {
-			continue;
-		}
-
-#if APP_WORKER_DROP_ALL_PACKETS
-		for (j = 0; j < bsz_rd; j ++) {
-			struct rte_mbuf *pkt = lp->mbuf_in.array[j];
-			rte_pktmbuf_free(pkt);
-		}
-
-		continue;
-#endif
-
-		APP_WORKER_PREFETCH1(rte_pktmbuf_mtod(lp->mbuf_in.array[0], unsigned char *));
-		APP_WORKER_PREFETCH0(lp->mbuf_in.array[1]);
-
-		for (j = 0; j < bsz_rd; j ++) {
-			//struct ipv4_hdr *ipv4_hdr;
-			//uint32_t ipv4_dst, pos;
-			//uint8_t port;
-
-			if (likely(j < bsz_rd - 1)) {
-				APP_WORKER_PREFETCH1(rte_pktmbuf_mtod(lp->mbuf_in.array[j+1], unsigned char *));
-			}
-			if (likely(j < bsz_rd - 2)) {
-				APP_WORKER_PREFETCH0(lp->mbuf_in.array[j+2]);
-			}
-
-			/*Obtenemos el paquete...*/
-			
-			/*se lo pasamos al trabajador*/
-			//ipv4_hdr = (struct ipv4_hdr *)(rte_pktmbuf_mtod(pkt, unsigned char *) + sizeof(struct ether_hdr));
-			//ipv4_dst = rte_be_to_cpu_32(ipv4_hdr->dst_addr);
-
-			/*Buscamos en la tabla LPM (que no lo hacemos realmente...)*/
-			/*if (unlikely(rte_lpm_lookup(lp->lpm_table, ipv4_dst, &port) != 0)) {
-				port = pkt->pkt.in_port;
-			}*/
-			
-		}
-	}
-}
-
-static inline void
-app_lcore_worker_flush(struct app_lcore_params_worker *lp)
-{
-	uint32_t port;
-
-	for (port = 0; port < APP_MAX_NIC_PORTS; port ++) {
-		int ret;
-
-		if (unlikely(lp->rings_out[port] == NULL)) {
-			continue;
-		}
-
-		if (likely((lp->mbuf_out_flush[port] == 0) ||
-		           (lp->mbuf_out[port].n_mbufs == 0))) {
-			lp->mbuf_out_flush[port] = 1;
-			continue;
-		}
-
-		ret = rte_ring_sp_enqueue_bulk(
-			lp->rings_out[port],
-			(void **) lp->mbuf_out[port].array,
-			lp->mbuf_out[port].n_mbufs);
-
-		if (unlikely(ret < 0)) {
-			uint32_t k;
-			for (k = 0; k < lp->mbuf_out[port].n_mbufs; k ++) {
-				struct rte_mbuf *pkt_to_free = lp->mbuf_out[port].array[k];
-				rte_pktmbuf_free(pkt_to_free);
-			}
-		}
-
-		lp->mbuf_out[port].n_mbufs = 0;
-		lp->mbuf_out_flush[port] = 1;
-	}
-}
-
-static void
-app_lcore_main_loop_worker(void) {
-	uint32_t lcore = rte_lcore_id();
-	struct app_lcore_params_worker *lp = &app.lcore_params[lcore].worker;
-	uint64_t i = 0;
-
-	uint32_t bsz_rd = app.burst_size_worker_read;
-
-	for ( ; ; ) {
-		/*if (APP_LCORE_WORKER_FLUSH && (unlikely(i == APP_LCORE_WORKER_FLUSH))) {
-			app_lcore_worker_flush(lp);
-			i = 0;
-		}*/
-
-		app_lcore_worker(lp, bsz_rd);
 
 		i ++;
 	}
@@ -650,19 +465,6 @@ app_lcore_main_loop(__attribute__((unused)) void *arg)
 		printf("Logical core %u (I/O) main loop.\n", lcore);
 		app_lcore_main_loop_io();
 	}
-
-	if (lp->type == e_APP_LCORE_WORKER) {
-		printf("Logical core %u (worker %u) main loop.\n",
-			lcore,
-			(unsigned) lp->worker.worker_id);
-		app_lcore_main_loop_worker();
-	}
 	
-	/** External **/
-	if (lp->type == e_APP_LCORE_WORKER_SLAVE) {
-		printf("Logical core %u (worker slave) main loop.\n",
-			lcore);
-		//external_slave();
-	}
 	return 0;
 }
