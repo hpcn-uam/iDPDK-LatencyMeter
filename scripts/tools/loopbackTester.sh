c07ac5d331a026dea3a4731924e6d02f03a07da0
Rafael Leira Osuna <rafael.leira@uam.es>
2016-10-25 11:30:30 +0200

    bugfix: outputfilename was wrong

--- scripts/tools/loopbackTester.sh
+++ scripts/tools/loopbackTester.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PKTSIZES="60 64 70 80 90 98 100 128 256 512 1024 1514"
 TRAINLENS="100 1000 10000"
 TRAINSLEEPS="0 64 128 1024"
-WAITTIME="10000000000 1000000000"
+WAITTIME="1000000000 10000000000"
 
 for waittime in $WAITTIME ; do
     for trainsleep in $TRAINSLEEPS ; do
@@ -14,7 +14,7 @@ for waittime in $WAITTIME ; do
             rm -f results_$waittime_$trainsleep_$trainlen.txt
             for pktsize in $PKTSIZES ; do
                 echo "Test $waittime $trainsleep $trainlen $pktsize"
-                $DIR/scriptExecuter.sh --trainLen $trainlen --trainSleep $trainsleep --waitTime $waittime --pktLen $pktsize | tee -a results_$waittime_$trainsleep_$trainlen.txt
+                $DIR/scriptExecuter.sh --trainLen $trainlen --trainSleep $trainsleep --waitTime $waittime --pktLen $pktsize | tee -a results.$waittime.$trainsleep.$trainlen.txt
             done
         done
     done
