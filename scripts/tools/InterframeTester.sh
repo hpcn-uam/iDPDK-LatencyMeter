3148f07850ff68d416685765ab211f383a5f8a9b
Rafael Leira Osuna <rafael.leira@uam.es>
2017-02-09 18:09:38 +0100

    interframe tester bugfix

--- scripts/tools/InterframeTester.sh
+++ scripts/tools/InterframeTester.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PKTSIZES="60 64 70 80 90 98 100 128 256 512 1024 1514"
 TRAINLENS="1000"
-TRAINSLEEPS="$(seq 0 2000 10)"
+TRAINSLEEPS="$(seq 0 10 2000)"
 
 for trainsleep in $TRAINSLEEPS ; do
     for trainlen in $TRAINLENS ; do
