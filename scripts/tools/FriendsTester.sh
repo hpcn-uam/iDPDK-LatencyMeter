8b4b117aec6694ec05c185611bad8bc9b8cb1cd9
Rafael Leira Osuna <rafael.leira@uam.es>
2017-02-13 12:43:16 +0100

    updated friendtester feedback

--- scripts/tools/FriendsTester.sh
+++ scripts/tools/FriendsTester.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PKTSIZES="60 64 70 80 90 98 100 128 256 512 1024 1514"
 TRAINLENS="1000"
-TRAINSLEEPS="$(seq 0 10 2000)"
+TRAINSLEEPS="$(seq 0 50 2000)"
 TRAINFRIENDS="$(seq 0 70)"
 
 for trainfriends in $TRAINFRIENDS ; do
@@ -14,7 +14,7 @@ for trainfriends in $TRAINFRIENDS ; do
         for trainlen in $TRAINLENS ; do
             rm -f $RESULTBASE/results.$trainsleep.$trainlen.txt
             for pktsize in $PKTSIZES ; do
-                echo "Test $trainsleep $trainlen $pktsize"
+                echo "Test Friend=$trainfriends Sleep=$trainsleep Train=$trainlen Len=$pktsize"
                 $DIR/scriptExecuter.sh --trainLen $trainlen --trainSleep $trainsleep --pktLen $pktsize --trainFriends $trainfriends | tee -a $RESULTBASE/results.$trainsleep.$trainlen.txt
             done
         done
