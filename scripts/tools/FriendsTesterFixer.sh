ce4b3f1ea055232a8f722cd40107fc17e5d3d2c5
Rafael Leira Osuna <rafael.leira@uam.es>
2017-02-21 14:49:22 +0100

    Added friend tester fixer, to re-do test with some kind of fail

---
+++ scripts/tools/FriendsTesterFixer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PKTSIZES="60 64 70 80 90 98 100 128 256 512 1024 1514"
+TRAINLENS="1000"
+TRAINSLEEPS="$(seq 0 50 1500)"
+TRAINFRIENDS="$(seq 0 70)"
+
+for trainfriends in $TRAINFRIENDS ; do
+    export RESULTBASE="$DIR/results/friendly/$trainfriends/"
+    for trainsleep in $TRAINSLEEPS ; do
+        for trainlen in $TRAINLENS ; do
+            for pktsize in $PKTSIZES ; do
+                OUTPARAMS="--trainLen $trainlen --trainSleep $trainsleep --pktLen $pktsize --trainFriends $trainfriends"
+                #echo "Test Friend=$trainfriends Sleep=$trainsleep Train=$trainlen Len=$pktsize"
+                if [ -f "$OUTPARAMS.txt" ]; then
+
+                else
+                    echo "Redoing test (not exists) Friend=$trainfriends Sleep=$trainsleep Train=$trainlen Len=$pktsize"
+                    $DIR/scriptExecuter.sh --trainLen $trainlen --trainSleep $trainsleep --pktLen $pktsize --trainFriends $trainfriends
+                    rm -f $RESULTBASE/results.$trainsleep.$trainlen.txt
+                fi
+            done
+        done
+    done
+done
