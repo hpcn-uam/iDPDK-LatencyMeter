1e747d3f11f60168e37efc83ffdf229d6598ce72
Rafael Leira Osuna <rafael.leira@uam.es>
2017-02-21 15:08:56 +0100

    Limited the friendTesterFixer scope

--- scripts/tools/FriendsTesterFixer.sh
+++ scripts/tools/FriendsTesterFixer.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PKTSIZES="60 64 70 80 90 98 100 128 256 512 1024 1514"
 TRAINLENS="1000"
 TRAINSLEEPS="$(seq 0 50 1500)"
-TRAINFRIENDS="$(seq 0 70)"
+TRAINFRIENDS="$(seq 0 9) $(seq 10 5 70)"
 
 for trainfriends in $TRAINFRIENDS ; do
     export RESULTBASE="$DIR/results/friendly/$trainfriends/"
